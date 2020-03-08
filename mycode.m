2ccac110748487909ff846356bc0b18eb14be8b0
Eddie_zy <zhangy990630@gmail.com>
2020-03-09 10:15:23 +1100

    prepare second version

--- mycode.m
+++ mycode.m
@@ -1,18 +1,4 @@
 
-% Example program, for off-line processing of saved laser scans. 
-% Example source code, useful for Parst A and B of project 01.. 
-% AAS - 2020.T1
-% Jose Guivant.
-
-% Note: Plotting of results is implemented, here, via a brute force approach.
-% See other provided examples, for better implementation (dynamic updates using "set()").
-% I expect you to avoid "brute force" implementations, in your projects. 
-
-
-
-
-
-
 function MyProgram(DataFileName)
 
 clc(); close all;
@@ -20,7 +6,7 @@ clc(); close all;
 % In case the caller does not specify the input argument, we propose a default one.
 if ~exist('DataFileName','var'), DataFileName ='Laser__2.mat'; end;
 
-% load data file.
+% load data     file.
 load(DataFileName); 
 % The variable that contains the data structure is named "dataL"
 % (because it was created under that name and saved)  
@@ -71,32 +57,41 @@ Y = sin(angles).*ranges;
 
 % Plot. "BRUTE FORCE" plot (see note 1).
 figure(1) ; clf(); hold on;
-plot(X,Y,'b.');                     % all points
+MyGUIHandles.bluePlot = plot(X,Y,'b.');
+               
 ii = find(intensities~=0);          % find those "pixels" that had intense reflection (>0) (aka: Highly Reflective pixels, HR)
-plot(X(ii),Y(ii),'+r');             % plot highly reflective ones
+MyGUIHandles.RedPlot = plot(X(ii),Y(ii),'+r');             % plot highly reflective ones
 axis([-10,10,0,20]);                % focuses plot on this region ( of interest in L220)
-
+MyGUIHandles.GreenPlot = plot(0,0,"g*");
 % To be done (by you)
 OOIs = ExtractOOIs(ranges,intensities) ;
-PlotOOIs(OOIs);
+PlotOOIs(OOIs,MyGUIHandles);
 
 
 return;
 end
 function r = ExtractOOIs(ranges,intensities)
     
+    global temp 
     temp = [];%for index
+    global temp1 
     temp1 =[];%for corresponding range value
+    global num;
     num = 1;
     r.N = 1;
     r.Centers= [];
     r.Sizes=[];
+    global scanNum;
     scanNum = length(intensities);
     for n = 1:scanNum
         if intensities(n)>0 
             temp(num)= n;
             temp1(num)=ranges(n);
+            
             num = num+1;
+            findNearByponits(n,ranges,num,intensities);
+            
+            
         end
         if (intensities(n)==0 && ~isempty(temp))
             %r.Centers = objectDetail(temp,temp1);
@@ -120,7 +115,7 @@ function r = ExtractOOIs(ranges,intensities)
 return;
 end
 function  info = objDetail(points,pointsRange)
-coor = [];
+
 angles = (points-1).*0.5.* pi./180 ;
 X = cos(angles).*pointsRange;
 Y = sin(angles).*pointsRange;
@@ -134,26 +129,76 @@ dia = sqrt(max((X-cx).^2+(Y-cy).^2));
 info = [cx,cy,dia];
 
 end
+function findNearByponits(num,r,num1,i)
+    global temp;
+    global temp1;
+    global scanNum;
+    constraint = 0.3;
+    currentAngle = num;
+    currentLength = r(num);
+    for s =num-1:-1:1
+       
+        if(EulDistance(s,r(s),currentAngle,currentLength)<=constraint && i(s)==0 )
+            temp(num1) = s;
+            temp1(num1) = r(s);
+        elseif(i(s)>0)
+            continue;
+        else
+            break;
+        end
+    end
+    
+    for s =num+1:1:scanNum
+        if(EulDistance(s,r(s),currentAngle,currentLength)<=constraint&& i(s)==0 )
+            temp(num1) = s;
+            temp1(num1) = r(s);
+        elseif(i(s)>0)
+            continue;
+        else
+            break;
+        end
+    end
+end
+function d =  EulDistance(a1,l1,a2,l2)
+angle1 = a1*0.5* pi/180;
+angle2 = a1*0.5* pi/180;
 
+X1 = cos(angle1).*l1;
+Y1 = sin(angle1).*l1;
 
+X2 = cos(angle2).*l2;
+Y2 = sin(angle2).*l2;
+
+d = sqrt((X1-X2)^2+(Y1-Y2)^2);
+disp(d);
+
+end
 
 %Randy bullock's paper
 function center = CenterFind(X,Y)
-    x = mean(X);
-    y = mean(Y);
+%     x = mean(X);
+%     y = mean(Y);
+      x = (max(X)+min(X))/2;
+      y = (max(Y)+min(Y))/2;
 
      center = [x;y];
     return ;
 end
 
 
-function PlotOOIs(OOIs)
+function PlotOOIs(OOIs,handler)
     if OOIs.N<1, return ; end
     % your part....
-    for n = 1:OOIs.N
-        c = OOIs.Centers(n);
-       plot(c.x,c.y,"g*")
-    end
+%     for n = 1:OOIs.N
+%         c = OOIs.Centers(n);
+% %        MyGUIHandles.GreenPlot = plot(c.x,c.y,"g*")
+%        set(handler.GreenPlot,'xdata',c.x,'ydata',c.y);
+%     end
+    table = struct2table(OOIs.Centers())
+    x  = table.x;
+    y = table.y;
+    set(handler.GreenPlot,'xdata',x,'ydata',y);
+  
 return;
 end
 
